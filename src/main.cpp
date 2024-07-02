221f00f24024ee8f2cbdeaf75908aab7577a0d02
BlackzByt3 <blackzspace.de@outlook.de>
2024-07-02 22:27:31 +0200

    Completed All in One Control!

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,12 @@
 #define LED_3 D4 // GPIO 2
 
 
+uint8_t LED1pin = D1;
+bool LED1status = LOW;
+
+uint8_t LED2pin = D6;
+bool LED2status = LOW;
+
 const char* ssid     = "BLACKLEAKZ-AP";
 const char* password = "123456789";
 
@@ -26,7 +32,7 @@ AsyncWebServer server(80);
 const char index_html[] PROGMEM = R"rawliteral(
   <!DOCTYPE HTML><html>
   <head>
-  <title>ESP Web Server</title>
+  <title>LED-Picture || Control Panel</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="data:,">
   <style>
@@ -43,14 +49,54 @@ const char index_html[] PROGMEM = R"rawliteral(
   </style>
 </head>
 <body>
+
   <h2>ESP Web Server</h2>
+
  <a href="/update">
   <center><button>Light ON</button></center>
 </a>
 
+<center><p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a></center>
+<center><p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a></center>
+<center><p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a></center>
+<center><p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a></center>
 </body>
 </html>
 )rawliteral";
+
+
+String SendHTML(uint8_t led1stat,uint8_t led2stat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #1abc9c;}\n";
+  ptr +=".button-on:active {background-color: #16a085;}\n";
+  ptr +=".button-off {background-color: #34495e;}\n";
+  ptr +=".button-off:active {background-color: #2c3e50;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>ESP8266 Web Server</h1>\n";
+  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
+  
+   if(led1stat)
+  {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";}
+
+  if(led2stat)
+  {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
 
 
 
@@ -85,6 +131,9 @@ void setup() {
   delay(300);
   digitalWrite(LED_3, HIGH);
 
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+
 
 
   if(!LittleFS.begin()){
@@ -106,14 +155,53 @@ void setup() {
   Serial.println(WiFi.localIP());
 
 
+ //server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  //  request->send_P(200, "text/html", index_html);
+  //});
 
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
+    request->send(200, "text/html", SendHTML(LED1status,LED2status));
+      LED1status = LOW;
+      LED2status = LOW;
+      Serial.println("GPIO7 Status: OFF | GPIO6 Status: OFF");
+
   });
 
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+ 
 
+  server.on("/led1on", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", SendHTML(true,LED2status));
+    LED1status = HIGH;
+    Serial.println("GPIO7 Status: ON");
+  });
+
+
+
+  server.on("/led1off", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", SendHTML(false,LED2status));
+    LED1status = LOW;
+    Serial.println("GPIO7 Status: OFF");
+  });
+  
+  server.on("/led2on", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", SendHTML(LED1status,true));
+    LED1status = HIGH;
+    Serial.println("GPIO7 Status: ON");
+  });
+
+
+
+  server.on("/led2off", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", SendHTML(LED1status,false));
+    LED1status = LOW;
+    Serial.println("GPIO7 Status: OFF");
+  });
+  
+
+
+
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
       digitalWrite(LED_1, LOW);
       delay(500);
       digitalWrite(LED_1, HIGH);
@@ -147,10 +235,16 @@ void setup() {
 void loop(){  
 
 
-
-  digitalWrite(LED_BUILTIN, LOW); 
-  delay(1000);                      
-  digitalWrite(LED_BUILTIN, HIGH); 
-  delay(2000);
-
+if(LED1status)
+  {digitalWrite(LED1pin, HIGH);}
+  else
+  {digitalWrite(LED1pin, LOW);}
+  
+  if(LED2status)
+  {digitalWrite(LED2pin, HIGH);}
+  else
+  {digitalWrite(LED2pin, LOW);}
 }
+
+
+
